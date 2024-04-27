3a8adcafee0366cd892e684649d35cc311e8c292
Hoang Lan <tranhoanglan1203@gmail.com>
2024-04-27 09:53:48 +0700

    update IsPrimeAsyce func

--- Ex2.cs
+++ Ex2.cs
@@ -11,7 +11,7 @@ class Program
         if (number <= 1)
             return false;
 
-        for (int i = 2; i < number / 2; i++)
+        for (int i = 2; i <= Math.Sqrt(number); i++)
         {
             if (number % i == 0)
                 return false;
